971ee7a498fc1ad66f7ba2e359678f64dffcde9a
Hugo Dias <hugo.victor.dias.teodoro@gmail.com>
2012-07-14 16:23:02 -0300

    init

--- app/Model/User.php
+++ app/Model/User.php
@@ -1,46 +1,44 @@
 <?php 
-/**
-* 
-*/
 App::uses('AuthComponent', 'Controller/Component');
+
 class User extends AppModel
 {
-	public $name = 'User';
+    public $name = 'User';
 	
-	public function beforeSave() 
-	{
-	    if (isset($this->data[$this->alias]['password'])) 
-	    {
-	        $this->data[$this->alias]['password'] = AuthComponent::password($this->data[$this->alias]['password']);
-	    }
-	    return true;
-	}
+  public function beforeSave() 
+  {
+    if (isset($this->data[$this->alias]['password'])) 
+    {
+      $this->data[$this->alias]['password'] = AuthComponent::password($this->data[$this->alias]['password']);
+    }
+    return true;
+  }
 
-    public $validate = array(
-        'username' => array(
-            array(
-                'rule' => array('notEmpty'),
-                'message' => 'A username is required.'
-            ),
-            array(
-                'rule' => 'isUnique',
-                'message' => 'This user already exists.'
-            )
-        ),
-        'password' => array(
-            'required' => array(
-                'rule' => array('notEmpty'),
-                'message' => 'A password is required',
-                'on' => 'create'
-            )
-        ),
-        'role' => array(
-            'valid' => array(
-                'rule' => array('inList', array('admin', 'author')),
-                'message' => 'Please enter a valid role',
-                'allowEmpty' => false
-            )
-        )
-    );    
+  public $validate = array(
+    'username' => array(
+      array(
+        'rule' => array('notEmpty'),
+        'message' => 'A username is required.'
+      ),
+      array(
+        'rule' => 'isUnique',
+        'message' => 'This user already exists.'
+      )
+    ),
+    'password' => array(
+      'required' => array(
+        'rule' => array('notEmpty'),
+        'message' => 'A password is required',
+        'on' => 'create'
+      )
+    ),
+    'role' => array(
+      'valid' => array(
+        'rule' => array('inList', array('admin', 'author')),
+        'message' => 'Please enter a valid role',
+        'allowEmpty' => false
+      )
+    )
+  );    
 }
 ?>
